359ab96301e09572a1a4adb0205592aeb116a26b
KrzaQ <git@debil.eu>
2013-05-06 01:53:06 +0200

    Back to globals, at least it works :(

--- main.rb
+++ main.rb
@@ -3,6 +3,6 @@ require File.dirname(__FILE__)+'/pokemensch.rb'
 
 eval(File.read('config.rb'))
 
-bot = PokeMensh.new(ConfigData)
+bot = PokeMensch.new(ConfigData)
 
 bot.start
--- pokemensch.rb
+++ pokemensch.rb
@@ -1,26 +1,25 @@
 require 'cinch'
 require 'csv'
 
-class PokeMensh
+class PokeMensch
 
 	def initialize(conf)
 	
-		@data = {}
+		$data = {}
 	
 		CSV.foreach("db.csv", :headers => false, :converters => :all) do |row|
-		  @data[row[0]] = row[1]
+		  $data[row[0]] = row[1].downcase
 		end
 
-		@ison = false
-		
 		@bot = Cinch::Bot.new do
-			@sleeptime = conf[:wait].to_f
-
+		
 			configure do |c|
 				c.server = conf[:server]
 				c.channels = conf[:channels]
 				c.nick = conf[:nick]
 				c.user = conf[:user]
+				
+				$sleeptime = conf[:wait].to_f
 			end
 			
 			on :message, ".on" do |m|
@@ -33,31 +32,31 @@ class PokeMensh
 			
 			on :message, /.{0,5}Question.+umerical.+/ do |m|
 				if @ison == true
-					sleep @sleeptime
+					sleep $sleeptime
 					m_o = m.message.gsub(/[^A-Za-z ]/, '')
 					m_o.gsub!(/[0-9]/,'')
 					pokemang = m_o.split[1].downcase
 					pokemang = pokemang[/([a-z]+)/]
-					m.reply "#{@data.key(pokemang)}"
+					m.reply "#{$data.key(pokemang)}"
 				end
 			end
 			
 			on :message, /.{0,5}Question.{0,6}/ do |m|
 				if @ison == true
-					sleep @sleeptime
+					sleep $sleeptime
 					m_o = m.message.gsub(/[^A-z0-9 ]/, '')
-					m.reply "#{@data[m_o.split[1].to_i]}"
+					m.reply "#{$data[m_o.split[1].to_i]}"
 				end
 			end
 		end
+	
 	end
 	
 	def start
-		@ison = true
 		@bot.start
 	end
 	
 	def stop
-		@ison = false
+		@bot.stop
 	end
 end
